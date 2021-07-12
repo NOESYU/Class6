c1161426dca81f390541ca1e859432781e62e79c
NOESYU <ssunuu114@gmail.com>
2021-07-12 12:05:51 +0900

    210712 django class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,12 +12,16 @@ def hello_world(request):
         new_hello_world = HelloWorld() #새로운객체만들고
         new_hello_world.text = temp
         new_hello_world.save() #DB안에 저장하고 아래서 불러오는건 저장된걸 불러옴
+
+        #이제 단일객체 읽는게 아닌 모든 객체 불러읽기 get는 단일 all은 전체
+        hello_list = HelloWorld.objects.all() #db에 저장된 내용들을 all가져옴
         return render(request, 'accountapp/hello_world.html',
-                      context={'new_hello_world': new_hello_world})
+                      context={'hello_list': hello_list})
 
     elif request.method == 'GET':
+        hello_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'Get Method'})
+                      context={'hello_list' : hello_list})
 
 
 
