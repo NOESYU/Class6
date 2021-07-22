ce0086bc5818b89479ece543ade992a4524ccf6c
NOESYU <ssunuu114@gmail.com>
2021-07-22 12:27:33 +0900

    210722 django class commit 3

--- django_0629/settings.py
+++ django_0629/settings.py
@@ -136,11 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #특정앱에 연결되지않은 정적 경로
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+MEIDA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
