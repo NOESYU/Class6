271e8ce4353b29e258903486e103ac73ac85a40a
NOESYU <ssunuu114@gmail.com>
2021-07-27 18:08:21 +0900

    210727 django class commit 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -19,3 +19,11 @@ class ProfileCreateView(CreateView):
         form.instance.user = self.request.user # 유저와맞는지 서버에서 받아와 비교
         # form은 image, nickname, message만 갖고있으니까 instance에 있는 user와 비교
         return super().form_valid(form)
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
