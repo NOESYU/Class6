47ae85711ea62ddae193bdc4a8829302d2219f1d
NOESYU <ssunuu114@gmail.com>
2021-07-29 11:58:44 +0900

    210729 django class commit 1&2

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,17 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+# 데코레이터로 접근 제한하기
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     #오버라이딩
@@ -20,10 +25,17 @@ class ProfileCreateView(CreateView):
         # form은 image, nickname, message만 갖고있으니까 instance에 있는 user와 비교
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self): # Mypage 띄우고싶은데 이전방법으로는 pk값을 바로 할수없어서 self.object.user = target_user
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
