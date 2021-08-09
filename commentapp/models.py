5b232d71c21907fb92d262d4fe35f5570f45cf63
NOESYU <ssunuu114@gmail.com>
2021-08-09 11:27:46 +0900

    210809 django class commit 2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True) # 본문(아티클)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True) # 작성자
+    content = models.TextField(null=False) # 내용
+    created_at = models.DateTimeField(auto_now_add=True) # 게시힌 날짜
