24add08a7591055073c80573fe4ba75be418f640
NOESYU <ssunuu114@gmail.com>
2021-08-10 18:01:38 +0900

    210810 django class commit 4 ngrok연결

--- django_0629/settings.py
+++ django_0629/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env_list['SECRET_KEY'] #.env에서 해당 value 받아오기
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
