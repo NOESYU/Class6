0412716a54e925ef65f97653397765bba77e6aeb
NOESYU <ssunuu114@gmail.com>
2021-08-12 11:44:36 +0900

    210812 django class commit 3

--- django_0629/urls.py
+++ django_0629/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('profile/', include('profileapp.urls')),
     path('article/', include('articleapp.urls')),
     path('comment/', include('commentapp.urls')),
+    path('project/', include('projectapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # 미디어 제공 해주는 경로 추가, django static conf 로 된거 import
