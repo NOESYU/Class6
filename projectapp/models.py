e435baf9389570abec92ccd93ab5d28ee2fae89a
NOESYU <ssunuu114@gmail.com>
2021-08-19 11:13:47 +0900

    210819 django class commit 1

--- projectapp/models.py
+++ projectapp/models.py
@@ -8,3 +8,8 @@ class Project(models.Model):
     description = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='project/', null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    # DB를 바꾸는게 아니니까 migrations 할 필요 없음!
+    def __str__(self): # 프로젝트 객체 출력될때 글(이름) 출력되는거 설정
+        return f'{self.name}'
