00063d98431a6502628e3d526c280e628ff32f85
NOESYU <ssunuu114@gmail.com>
2021-08-30 11:16:27 +0900

    210830 django class commit 1

--- articleapp/models.py
+++ articleapp/models.py
@@ -9,8 +9,9 @@ class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     # 프로젝트(게시판)와 아티클 연결용
+    # null 은 디비에 비어있어도 된다는 느낌, blank 는 ㄹㅇ 개시할때 입력안해도 되는 것!
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
